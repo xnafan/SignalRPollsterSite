c2acc89b085db29fc43a2dbdd7a91ff18bea6900
Jakob Farian Krarup <kr@rup.dk>
2022-11-05 00:18:00 +0100

    Working - done for the night. Simplified and commented javascript

--- SignalRPollsterSite/Hubs/PollHub.cs
+++ SignalRPollsterSite/Hubs/PollHub.cs
@@ -4,7 +4,7 @@ public class PollHub : Hub
 {
     private IPollProvider _pollProvider;
     public PollHub(IPollProvider pollProvider) => _pollProvider = pollProvider;
-    public async Task GetPollInfo(string pollId) => await Clients.Caller.SendAsync("ReceivePollInfo", _pollProvider.GetPollInfo(pollId));
-    public async Task CreatePoll(Poll poll) => await Clients.Caller.SendAsync("PollCreated", _pollProvider.CreatePoll(poll));
-    public async Task Vote(string pollId, int vote) => await Clients.All.SendAsync("VoteUpdated", _pollProvider.Vote(pollId, vote));
+    public async Task GetPollInfo(string pollId) => await Clients.Caller.SendAsync("receivePollInfo", _pollProvider.GetPollInfo(pollId));
+    public async Task CreatePoll(Poll poll) => await Clients.Caller.SendAsync("pollCreated", _pollProvider.CreatePoll(poll));
+    public async Task Vote(string pollId, int vote) => await Clients.All.SendAsync("voteUpdated", _pollProvider.Vote(pollId, vote));
 }
--- SignalRPollsterSite/wwwroot/pollster.js
+++ SignalRPollsterSite/wwwroot/pollster.js
@@ -3,43 +3,44 @@ let pollId = "4ER-8IK";
 var connection = new signalR.HubConnectionBuilder().withUrl("/pollster").build();
 
 //Disable the send button until connection is established.
-document.querySelectorAll(".voteButton").forEach((elem) => elem.disabled = true);
-connection.on("VoteUpdated", function (results) {
-    update(results);
-});
-connection.on("ReceivePollInfo", function (poll) {
-    updatePollInfo(poll);
-});
-
-connection.start().then(function () {
-    getUpdatedPollInfo();
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+//and hook up to the button click event
 document.querySelectorAll(".voteButton").forEach(
     (elem, index) => {
-        elem.disabled = false;
-        elem.addEventListener("click", function (event) {
+        elem.disabled = true;
+        elem.addEventListener("click", function () {
             connection.invoke("Vote", pollId, index).catch(function (err) {
                 return console.error(err.toString());
             });
-            event.preventDefault();
         });
     }
 );
-function update(results) {
+
+//HOOK UP SIGNALS FROM SERVER TO LOCAL METHODS:
+//updates UI after votes from any client
+connection.on("voteUpdated", updateResults);
+//is retrieved when page is first opened, to get current poll results
+connection.on("receivePollInfo", receivePollInfo);
+
+//retrieve current poll status from server when connection is up
+connection.start().then(function () {
+    getUpdatedPollInfo();
+    //and enable all vote buttons
+    document.querySelectorAll(".voteButton").forEach((elem) => elem.disabled = false);
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+function getUpdatedPollInfo() {
+    connection.invoke("GetPollInfo", pollId).catch(function (err) {
+        alert("Error retrieving updated poll info");
+    });
+}
+
+function updateResults(results) {
     for (let i = 0; i < results.length; i++) {
         let span = document.getElementById("option" + i + "score");
         span.innerText = results[i];
     }
 }
 
-function updatePollInfo(poll) {
-    update(poll.results);
-}
-
-function getUpdatedPollInfo() {
-    connection.invoke("GetPollInfo", pollId).catch(function (err) {
-        return console.error(err.toString());
-    });
-}
+function receivePollInfo(poll) { updateResults(poll.results); }
